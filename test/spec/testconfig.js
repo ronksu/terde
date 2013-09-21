a9f1840ed50fd6f22f6e97058a3e7d4517d02d98
ceel <juha.paananen@nokkau.net>
2013-09-21 22:38:45 +0300

    Fix jQuery override screwing up mockjax.

--- test/spec/testconfig.js
+++ test/spec/testconfig.js
@@ -1,6 +1,7 @@
+define('jquery', [], function() { return jQuery; });
+
 require.config({
     paths: {
-        jquery: '../bower_components/jquery/jquery',
         // @TODO mock
         mapBox: 'http://api.tiles.mapbox.com/mapbox.js/v1.0.2/mapbox',
         knockout: '../bower_components/knockout/knockout-2.3.0.debug',
