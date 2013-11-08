6697325b362b0df8d4bb84418d5b315906a4c145
ceel <juha.paananen@nokkau.net>
2013-11-08 22:13:59 +0200

    Remove unnecessary bootstrap deps and apply minified jquery.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,52 +1,12 @@
 require.config({
     paths: {
         jquery: '../bower_components/jquery/jquery',
-        bootstrapAffix: '../bower_components/sass-bootstrap/js/affix',
-        bootstrapAlert: '../bower_components/sass-bootstrap/js/alert',
-        bootstrapButton: '../bower_components/sass-bootstrap/js/button',
-        bootstrapCarousel: '../bower_components/sass-bootstrap/js/carousel',
-        bootstrapCollapse: '../bower_components/sass-bootstrap/js/collapse',
-        bootstrapPopover: '../bower_components/sass-bootstrap/js/popover',
-        bootstrapScrollspy: '../bower_components/sass-bootstrap/js/scrollspy',
-        bootstrapTab: '../bower_components/sass-bootstrap/js/tab',
-        bootstrapTooltip: '../bower_components/sass-bootstrap/js/tooltip',
-        bootstrapTransition: '../bower_components/sass-bootstrap/js/transition',
         knockout: '../bower_components/knockout.js/knockout',
         knockoutMapping: '../bower_components/knockout-mapping/knockout.mapping',
         lodash: '../bower_components/lodash/dist/lodash.min',
         Uri: '../bower_components/jsUri/Uri.min'
     },
     shim: {
-        bootstrapAffix: {
-            deps: ['jquery']
-        },
-        bootstrapAlert: {
-            deps: ['jquery']
-        },
-        bootstrapButton: {
-            deps: ['jquery']
-        },
-        bootstrapCarousel: {
-            deps: ['jquery']
-        },
-        bootstrapCollapse: {
-            deps: ['jquery']
-        },
-        bootstrapPopover: {
-            deps: ['jquery']
-        },
-        bootstrapScrollspy: {
-            deps: ['jquery']
-        },
-        bootstrapTab: {
-            deps: ['jquery']
-        },
-        bootstrapTooltip: {
-            deps: ['jquery']
-        },
-        bootstrapTransition: {
-            deps: ['jquery']
-        },
         Uri: {
             exports: 'Uri'
         }
