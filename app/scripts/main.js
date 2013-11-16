017415a2e3ab828728954910fbb467b2933d7205
ceel <juha.paananen@nokkau.net>
2013-11-16 16:43:14 +0200

    Add fastclick.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,8 @@ require.config({
         knockout: '../bower_components/knockout.js/knockout',
         knockoutMapping: '../bower_components/knockout-mapping/knockout.mapping',
         lodash: '../bower_components/lodash/dist/lodash.min',
-        Uri: '../bower_components/jsUri/Uri.min'
+        Uri: '../bower_components/jsUri/Uri.min',
+        fastclick: '../bower_components/fastclick/lib/fastclick'
     },
     shim: {
         Uri: {
@@ -17,6 +18,7 @@ require.config({
     }
 });
 
-require(['app'], function (app) {
+require(['fastclick', 'app'], function (fastclick, app) {
+    fastclick.attach(document.body);
     app.init();
 });
